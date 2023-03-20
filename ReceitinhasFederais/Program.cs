2dd898f8a02e3ce25feb25d31198f807cbf18be8
Renan <contactrenchaves@gmail.com>
2023-03-20 19:35:55 -0300

    adicionado a classe Receitas e a funcionalidade no program.cs com a criação da pasta e do arquivo que irá funcionar como Banco de Dados e a lista que irá desempenhar o papel de criar as receitas e adicionar no banco

--- ReceitinhasFederais/Program.cs
+++ ReceitinhasFederais/Program.cs
@@ -11,7 +11,35 @@ namespace ReceitinhasFederais
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            try
+            {
+                MessageBox.Show("Por favor aguarde, o programa está criando as pastas e arquivos necessários!");
+                if (Directory.Exists(caminhoPasta) && File.Exists(caminhoTXT))
+                {
+                    MessageBox.Show("A pasta já existe e o arquivo também!");
+                }
+                else
+                {
+                    DirectoryInfo PastaContatos = Directory.CreateDirectory(caminhoPasta);
+                    File.Create(caminhoTXT).Close();
+                    MessageBox.Show("A Pasta foi criada com sucesso e o arquivo também!");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Não foi possível concluir as operações de criar pasta ou arquivo.txt.\nErro: ", e.ToString());
+            }
+
             Application.Run(new MainMenu());
         }
+
+        //trocar por " y: " caso estiver na facul e nao funcionar o codigo abaixo
+        public static string caminhoTXT = @"c:\PastaReceitas\BancoDeReceitas.txt";
+
+        //trocar por " y: " caso estiver na facul e nao funcionar o codigo abaixo
+        public static string caminhoPasta = @"c:\PastaReceitas";
+
+        public static List<Receitas> ListaReceitas = new List<Receitas>();
     }
 }
---
+++ ReceitinhasFederais/Receitas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceitinhasFederais
+{
+    internal class Receitas
+    {
+        public string Titulo { get; set; }
+        public string Ingredientes { get; set; }
+        public string ModoPreparo { get; set; }
+        public string GrauDificuldade { get; set; }
+        public string Autor { get; set; }
+        public string TempoPreparo { get; set; }
+        public string Categoria { get; set; }
+        public int QntdPratos { get; set; }
+
+        public Receitas() { }
+
+        public Receitas(string titulo, string ingredientes, string modoPreparo, string grauDificuldade, string autor, string tempoPreparo, string categoria, int qntdpratos)
+        {
+            Titulo = titulo;
+            Ingredientes = ingredientes;
+            ModoPreparo = modoPreparo;
+            if (grauDificuldade == "Fácil")
+            {
+                GrauDificuldade = "Fácil";
+            }
+            else if (grauDificuldade == "Médio")
+            {
+                GrauDificuldade = "Médio";
+            }
+            else if (grauDificuldade == "Difícil")
+            {
+                GrauDificuldade = "Difícil";
+            }
+            else if (grauDificuldade == "Experiente")
+            {
+                GrauDificuldade = "Experiente";
+            }
+            Autor = autor;
+            TempoPreparo = tempoPreparo;
+            Categoria = categoria;
+            QntdPratos = qntdpratos;
+        }
+}
