5c7cd6edceb62d19978b35bb9d6dc6212bffc472
Renan <contactrenchaves@gmail.com>
2023-04-03 19:12:41 -0300

    Novo formulário adicionado
    Novo Form -> FormDuvidas
    
    Este formulário vai ter um botão simples que leva até ele para servir como um "guia" de como usar o programa, contendo perguntas e respostas para as possíveis dúvidas mais frequentes.

---
+++ ReceitinhasFederais/FormDuvidas.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ReceitinhasFederais
+{
+    partial class FormDuvidas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDuvidas));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Como faço para visualizar todas as receitas cadastradas?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(481, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Para visualizar todas as receitas cadastradas, basta você selecionar o filtro de " +
+    "dados \r\ne clicar na opção \"Tudo\" e digitar \"Tudo\" na pesquisa de dados e por fim" +
+    ", clicar\r\nno botão verde \"Pesquisar\".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(503, 51);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Para adicionar uma receita, basta clicar no botão \"Adicionar Receita\" no menu esq" +
+    "uerdo\r\ndo programa e preencher os dados necessários e por fim, clicar no botão v" +
+    "erde\r\n\"Adicionar\".\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Como faço para adicionar uma receita?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(347, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "____________________________________________________________________";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(12, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(502, 34);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Para remover uma receita, basta clicar no botão \"Excluir Receita\" no menu esquerd" +
+    "o do \r\nprograma e informar o [Título] da receita que deseja excluir.";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(12, 246);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(282, 20);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Como faço para remover uma receita?";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(12, 371);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 17);
+            this.label10.TabIndex = 9;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(12, 351);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(266, 20);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Como faço para editar uma receita?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(84, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(347, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "____________________________________________________________________";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(84, 336);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(347, 15);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "____________________________________________________________________";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(12, 371);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(491, 51);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Não é possível editar uma receita neste programa, o mais próximo que o usuário po" +
+    "de\r\nchegar de fazer isso é deletando a receita e adicionando uma nova sem comete" +
+    "r os \r\nerros";
+            // 
+            // FormDuvidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(520, 482);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormDuvidas";
+            this.Text = "Dúvidas";
+            this.Load += new System.EventHandler(this.FormDuvidas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label7;
+        private Label label8;
+        private Label label10;
+        private Label label11;
+        private Label label6;
+        private Label label9;
+        private Label label12;
+    }
+}
---
+++ ReceitinhasFederais/FormDuvidas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReceitinhasFederais
+{
+    public partial class FormDuvidas : Form
+    {
+        public FormDuvidas()
+        {
+            InitializeComponent();
+
+            //esses private doido ai é pq clicamos no formulario pra abrir esse codigo aq, mas n tem nenhuma programação especifica nele, é td label
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormDuvidas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
